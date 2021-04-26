3795f6383763fa5d681900d90a910f8624345b1c
ARYYAMA KUMAR JANA <janaaryyama@gmail.com>
2021-04-26 12:49:09 -0700

    Minor fixes

--- clean.py
+++ clean.py
@@ -11,3 +11,9 @@ for root, dirs, files in os.walk('./posenet_nodejs_setup-master/Posenet_Frames')
         os.unlink(os.path.join(root, f))
     for d in dirs:
         shutil.rmtree(os.path.join(root, d))
+        
+ for root, dirs, files in os.walk('./keypoints_Posenet'):
+    for f in files:
+        os.unlink(os.path.join(root, f))
+    for d in dirs:
+        shutil.rmtree(os.path.join(root, d))
