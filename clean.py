06c9c27c9ecb9041492f75625a132e6e53cdd8a5
ARYYAMA KUMAR JANA <janaaryyama@gmail.com>
2021-04-26 12:38:42 -0700

    Minor changes

--- clean.py
+++ clean.py
@@ -5,3 +5,9 @@ for root, dirs, files in os.walk('./handframes/Demo_frames'):
         os.unlink(os.path.join(root, f))
     for d in dirs:
         shutil.rmtree(os.path.join(root, d))
+
+for root, dirs, files in os.walk('./posenet_nodejs_setup-master/Posenet_Frames'):
+    for f in files:
+        os.unlink(os.path.join(root, f))
+    for d in dirs:
+        shutil.rmtree(os.path.join(root, d))
